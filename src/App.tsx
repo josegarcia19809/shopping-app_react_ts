c48b329c9769ebcf6527a20b3397dfc3095812a3
josegarcia19809 <josegarcia19809@gmail.com>
2025-01-17 14:25:07 -0600

    React & TypeScript ShoppingApp: Updating The State

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import ShoppingList from "./components/ShoppingList.tsx";
 import {useState} from "react";
 import Item from "./models";
 import ShoppingListForm from "./components/ShoppingListForm.tsx";
+import {v4 as getId} from "uuid";
 
 function App() {
 
     const [items, setItems] = useState<Item[]>([]);
 
     const addItem = (product: string) => {
-        console.log(product);
+        setItems([...items, {id: getId(), product, quantity: 1}])
     }
 
     return (
